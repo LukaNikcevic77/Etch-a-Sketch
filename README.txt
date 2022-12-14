b9a51f7be52293f19f3fae6718826e0616c29b5c
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2022-12-14 22:22:24 +0100

    Code cleanup and readme file

---
+++ README.txt
@@ -0,0 +1,9 @@
+Simple project made for foundation course of odin project.
+The goal was to build a web app that will look like and act like Etch a sketch.
+Main features asked for was to be able to "draw" when you hover over white surface,
+it was more logical to me to make it click and hover because the way it works in Etch A Sketch
+is similar to that. Required feature was to be able to change colors and size of the screen.
+Bonus feature was to add rainbow button that would randomly change between rainbow colors, 
+but rainbow was kinda casuall so I added my own pallet under name summer.
+
+This app was built without any frameworks with only plane javascript, html and css. 
--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
-const div = document.createElement("div");
 const summer = ["#0dafae", "#008284", "#094b4b", "#e8c4be", "#ac8885"];
+let pickedcolor = "#000000";
+let isDown = false;
+let summerize = false;
+let divider = 4;
+
+
 
 const graph = document.querySelector(".board");
 const buttonsummer = document.getElementById("summerVibe");
@@ -7,90 +12,107 @@ const colorpicker = document.getElementById("colorpicker");
 const sizemenu = document.getElementById("menu");
 
 
-let pickedcolor = "#000000";
-let isDown = false;
-let summerize = false;
+
+const div = document.createElement("div");
 
 
 
+document.addEventListener("mousedown", e = () => {
 
-let divider = 4;
+    isDown = true;
+
+})
+
+document.addEventListener("mouseup", e = () => {
+
+    isDown = false;
+
+})
 
 
-    div.style.maxWidth = 700 / divider + "px";
-    div.style.minWidth = 700 / divider + "px";
-    div.style.maxHeight = 700 / divider + "px";
-    div.style.minWidth = 700 / divider + "px";
+
+buttonsummer.addEventListener("click", e = () => {
+
+    summerize = true;
+
+})
+
+colorpicker.addEventListener("change", (e) => {
+
+    summerize = false;
+    pickedcolor = e.target.value;
+
+} )
+
+sizemenu.addEventListener("change", (e) => {
+
+    divider = e.target.value;
+    
+    changeGridSize();
+
+})
+
+
 
 changeGridSize();
 
-        document.addEventListener("mousedown", e = () => {
-            isDown = true;
-        })
-
-        document.addEventListener("mouseup", e = () => {
-            isDown = false;
-        })
-
-        
-
-       buttonsummer.addEventListener("click", e = () => {
-        summerize = true;
-        
-
-       })
-
-       colorpicker.addEventListener("change", (e) => {
-        summerize = false;
-        pickedcolor = e.target.value;
-
-       } )
-
-       sizemenu.addEventListener("change", (e) => {
-        divider = e.target.value;
-        console.log(e.target.value);
-        changeGridSize();
-       })
-
-       
-       
 
 
 function changeGridSize(){
-    while(graph.firstChild){
-        graph.removeChild(graph.firstChild);
-    }
-    document.documentElement.style.setProperty("--grid-size", divider);
     
-    let i = 0;
+    while(graph.firstChild){
+
+            graph.removeChild(graph.firstChild);
+    
+    }
+
+document.documentElement.style.setProperty("--grid-size", divider);
+    
+let i = 0;
     
     for(let i = 0; i < divider * divider; i++){
 
         let div = document.createElement("div");
-         div.setAttribute("draggable", false);
-        div.addEventListener("click", e  = () => {
-            if(summerize == true){
-                console.log(Math.floor(Math.random() * 4));
-                    div.style.backgroundColor = summer[Math.floor(Math.random() * 4)];
-            }
-            else {
-                div.style.backgroundColor = pickedcolor;
-            }
-        })
-        div.addEventListener("mouseover", e = () => {
+        div.setAttribute("draggable", false);
 
-            if(isDown == true){
-                if(summerize == true){
-                    console.log(Math.floor(Math.random() * 4));
-                    div.style.backgroundColor = summer[Math.floor(Math.random() * 4)];
-                }
-                else {
-                    div.style.backgroundColor = pickedcolor;
-                }
-           
-            }
+    div.addEventListener("click", e  = () => {
+
+    if(summerize == true){
             
-        })
+            //#function in [] returns whole number from 0 to 3
+
+        div.style.backgroundColor = summer[Math.floor(Math.random() * 4)];
+
+    }
+
+    else {
+
+        div.style.backgroundColor = pickedcolor;
+    
+    }
+
+})
+    div.addEventListener("mouseover", e = () => {
+
+    if(isDown == true){
+
+    if(summerize == true){
+
+            //#function in [] returns whole number from 0 to 3
+
+        div.style.backgroundColor = summer[Math.floor(Math.random() * 4)];
+                
+    }
+
+    else {
+
+        div.style.backgroundColor = pickedcolor;
+       
+    }
+           
+    }
+            
+})
         graph.appendChild(div);
         
     }
